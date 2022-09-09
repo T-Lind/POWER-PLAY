e6a9a215f5ca4bace30a9432f994aa2cb4796481
T-Lind <tiernanxklindauer@gmail.com>
2022-09-09 17:36:09 -0500

    disabled a teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestDiffyContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TestDiffyContainer.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
 import org.firstinspires.ftc.teamcode.teleop.support.DiffyTeleOpContainer;
 
+@Disabled
 public class TestDiffyContainer extends DiffyTeleOpContainer {
     @Override
     protected void initSpecificMechanisms() {
