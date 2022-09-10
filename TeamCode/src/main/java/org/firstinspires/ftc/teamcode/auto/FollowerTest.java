82e7ae5aa556c65c0952c496c39a06b58f67e801
T-Lind <tiernanxklindauer@gmail.com>
2022-09-10 17:02:49 -0400

    Created a program to run a drivetrain based on x, y and heading over time

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/FollowerTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.auto.differentialequationfollower.DefineEquations;
+
+public class FollowerTest extends LinearOpMode implements DefineEquations{
+    @Override
+    public double getX(double time) {
+        return 0;
+    }
+
+    @Override
+    public double getY(double time) {
+        return 0;
+    }
+
+    @Override
+    public double getHeading(double time) {
+        return 0;
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
