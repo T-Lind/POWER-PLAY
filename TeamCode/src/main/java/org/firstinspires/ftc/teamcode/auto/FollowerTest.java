6e3f69310d3b28b17c1bb410542e77d06b553d64
T-Lind <tiernanxklindauer@gmail.com>
2022-09-12 19:41:30 -0500

    updated the differential swerve equation follower

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/FollowerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/FollowerTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.auto;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.auto.differentialequationfollower.DefineEquations;
+import org.firstinspires.ftc.teamcode.auto.differentialequationfollower.DifferentialEquationFollower;
 
 public class FollowerTest extends LinearOpMode implements DefineEquations{
     @Override
@@ -22,6 +23,8 @@ public class FollowerTest extends LinearOpMode implements DefineEquations{
 
     @Override
     public void runOpMode() throws InterruptedException {
+        DifferentialEquationFollower follower = new DifferentialEquationFollower(this, hardwareMap);
+
 
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/differentialequationfollower/DifferentialEquationFollower.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/differentialequationfollower/DifferentialEquationFollower.java
@@ -16,6 +16,12 @@ public class DifferentialEquationFollower {
     private Motor backLeft;
     private Motor backRight;
 
+    double lastTime;
+    double lastVelMeasurement;
+
+    double endX;
+    double endY;
+    double endHeading;
 
     public DifferentialEquationFollower(DefineEquations equations, HardwareMap hardwareMap){
         this.equations = equations;
@@ -35,6 +41,25 @@ public class DifferentialEquationFollower {
         frontRight.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+
+        lastTime = 0;
+        lastVelMeasurement = 0;
+
+        double time = 0.1;
+        while(true){
+            double xPos = equations.getX(time);
+            double yPos = equations.getY(time);
+            double heading = equations.getHeading(time);
+
+            if(xPos == Double.MIN_VALUE && yPos == Double.MIN_VALUE && heading == Double.MIN_VALUE){
+                endX = xPos;
+                endY = yPos;
+                endHeading = heading;
+                break;
+            }
+
+            time += 0.001;
+        }
     }
 
     public void follow(){
@@ -44,16 +69,50 @@ public class DifferentialEquationFollower {
         PIDFController yPid = new PIDFController(3, 0.5, 0.5, 0.5);
         PIDFController headingPid = new PIDFController(3, 0.5, 0.5, 0.5);
 
+        lastTime = currentTime.milliseconds()/1000;
+
         while(true){
+            // Grab what the x, y, and heading should be at this instant in time
             double targetX = equations.getX(currentTime.milliseconds()/1000);
             double targetY = equations.getY(currentTime.milliseconds()/1000);
             double targetHeading = equations.getHeading(currentTime.milliseconds()/1000);
 
+            // Get the current x, y, and heading
             double currentX = localizer.getPoseEstimate().getX();
             double currentY = localizer.getPoseEstimate().getY();
             double currentHeading = localizer.getPoseEstimate().getHeading();
 
-//            xPid.calculate(currentX, targetX)
+            // Get the x, y, and heading corrections thru PIDF
+            double xCorrect = xPid.calculate(currentX, targetX);
+            double yCorrect = yPid.calculate(currentY, targetY);
+            double headingCorrect = headingPid.calculate(currentHeading, targetHeading);
+
+            // Get the heading the bot is going to be travelling in and the linear velocity it should be taking
+            double headingToMove = Math.atan2(xCorrect, yCorrect);
+            double linearVelocity = (Math.hypot(xCorrect, yCorrect)-lastVelMeasurement)/(currentTime.milliseconds()/1000-lastTime);
+
+            // TODO: Figure out the set of equations to convert this data into motor powers
+
+            lastTime = currentTime.milliseconds()/1000;
+
+            if(errorIsSmall(endX, endY, endHeading, currentX, currentY, currentHeading)){
+                break;
+            }
         }
+
+        frontLeft.set(0);
+        frontRight.set(0);
+        backLeft.set(0);
+        backRight.set(0);
+    }
+
+    private boolean errorIsSmall(double endX, double endY, double endHeading, double currentX, double currentY, double currentHeading){
+        double xError = Math.abs(endX-currentX);
+        double yError = Math.abs(endY-currentY);
+        double headingError = Math.abs(endHeading-currentHeading)/360;
+
+        // TODO: Figure out if dividing by 360 is enough to 'normalize'
+        //  the heading error to the same scale the x and y error is
+        return (xError + yError + headingError) / 3 < 0.1;
     }
 }
